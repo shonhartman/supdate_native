430cf690a3afcb7ad96f15052a448c373322f8be
shonhartman <shaun@bighuman.com>
2026-01-28 16:27:20 -0500

    Initial Commit

---
+++ supdate_native/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  supdate_native
+//
+//  Created by Shaun Hartman on 1/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ supdate_native/supdate_nativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  supdate_nativeApp.swift
+//  supdate_native
+//
+//  Created by Shaun Hartman on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct supdate_nativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
